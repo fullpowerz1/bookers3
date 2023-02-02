07a2da8483081ccf344bd6180bd8abb58d12b64c
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-02 08:17:47 +0000

    jin

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
